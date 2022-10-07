fbdb523dde6e173006b44196a3a278674480731d
ydalton <r0930164@student.thomasmore.be>
2022-10-07 17:13:19 +0000

    Added comments + added time limit to program.

--- ant.c
+++ ant.c
@@ -1,4 +1,23 @@
+/*
+ * This program is my implementation of Langton's Ant using OpenGL.
+ *
+ * It is similar to the Game of Life. The rules of the game are as follows: we start with a plane of squares that
+ * can either be black or white. One of these squares is known as the ant, who moves according to set of rules:
+ * - if the current square is white, turn 90 degrees clockwise and flip the color of the square.
+ * - if the current square is black, turn 90 degrees counter-clockwise and flip the color of the square.
+ *
+ * In any case the ant moves one square forward according to its current direction. The outcome of the game is
+ * completely deterministic if the initial conditions (i.e. the state of the map) are known. In an empty map, at
+ * around 11000 frames, the ant is stuck in an infinite loop until the program segfaults due to accessing a negative
+ * index in the map array.
+ *
+ * I added a random background generator which as of yet does not fully work: it generates a map but will generate
+ * the same map everytime.
+ *
+ */
+
 #include "ant.h"
+#include <GL/freeglut_std.h>
 #include <GL/glut.h>
 #include <stdbool.h>
 #include <stdio.h>
@@ -7,7 +26,7 @@
 
 bool Map[MAP_Y][MAP_X];
 Ant *pAnt;
-
+int id;
 
 void init(void) {
     glClearColor(WHITE, 0);
@@ -52,18 +71,19 @@ void drawAnt() {
     glEnd();
 }
 
+/* This following function contains the logic behind Langton's Ant. */
 void gameLoop(void) {
     for(int i = 0; i < FRAME_SKIP; i++) {
-        if (Map[pAnt->y][pAnt->x] == 0) {
+        if (Map[pAnt->y][pAnt->x] == 0) { /* If the current square is white: */
             Map[pAnt->y][pAnt->x] = 1;
             pAnt->direction++;
             if(pAnt->direction > 4)
-                pAnt->direction = 1;
+                pAnt->direction -= 4;
         } else {
             Map[pAnt->y][pAnt->x] = 0;
             pAnt->direction--;
             if(pAnt->direction < 1)
-                pAnt->direction = 4;
+                pAnt->direction += 4;
         }
         switch(pAnt->direction) {
             case 1:
@@ -85,10 +105,6 @@ void gameLoop(void) {
 }
 
 void display(void) {
-    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-    drawMap();
-    printf("%d\t%d\t%d\t%d\n", pAnt->x, pAnt->y, pAnt->direction, 0);
-    glutSwapBuffers();
     for(int i = 1; i < PROGRAM_LIMIT; i++) {
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
         drawMap();
@@ -97,19 +113,20 @@ void display(void) {
         printf("%d\t%d\t%d\t%d\n", pAnt->x, pAnt->y, pAnt->direction, i);
         glutSwapBuffers();
     }
+    glutDestroyWindow(id);
+    return;
 }
 
 int main(int argc, char *argv[]) {
     Ant a = {(MAP_X-1)/2, (MAP_Y-1)/2, 1}; /* Creates the ant. */
     pAnt = &a;
-    //genMap(50);
+    genMap(25); /* Uncomment this line to generate pseudorandom map for the ant. */
     glutInit(&argc, argv);
     glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB);
     glutInitWindowSize(W_WIDTH, W_HEIGHT);
-    glutCreateWindow(PROGRAM_NAME);
+    id = glutCreateWindow(PROGRAM_NAME);
     init();
     glutDisplayFunc(display);
     glutMainLoop();
-
     return 0;
 }
--- ant.h
+++ ant.h
@@ -1,21 +1,23 @@
 #ifndef ANT_H_
 #define ANT_H_
+
 /* COLORS */
 #define BLACK 0, 0, 0
 #define WHITE 1, 1, 1
 #define RED 1, 0, 0
+
 /* SIZES */
-#define PIXEL_SIZE 2
-#define STEP_SIZE 1
+#define PIXEL_SIZE 2 /* Pixel drawn size */
+#define STEP_SIZE 1 /* The size of the steps of the ant. */
 #define MAP_X 400
 #define MAP_Y 400
-#define FRAME_SKIP 10
+#define FRAME_SKIP 5 /* This defines the amount of frames that are skipped for every draw. */
 
 /* PROGRAM DEFINES */
-#define W_WIDTH MAP_X * PIXEL_SIZE
-#define W_HEIGHT MAP_Y * PIXEL_SIZE
-#define PROGRAM_NAME "Langton's Ant"
-#define PROGRAM_LIMIT 15000
+#define W_WIDTH MAP_X * PIXEL_SIZE /* Window width */
+#define W_HEIGHT MAP_Y * PIXEL_SIZE /* Window height */
+#define PROGRAM_NAME "Langton's Ant" /* Window name */
+#define PROGRAM_LIMIT 1500 /* This defines the amount of draws the program does before quitting. */
 
 typedef struct {
     unsigned int x,y;
